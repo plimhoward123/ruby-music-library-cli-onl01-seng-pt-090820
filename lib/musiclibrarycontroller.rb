c8330a9276805c7b1ace3c7a8182d587ebb85e5f
Plimon Howard <plimon.howard@gmail.com>
2020-10-22 19:44:16 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -58,8 +58,11 @@ class MusicLibraryController
 
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
-    if (Artist.find_by_name(gets.chomp) != nil)
-
+    artist = Artist.find_by_name(gets.chomp)
+    if (artist != nil)
+      tmp = artist.songs.sort_by { |songs| songs.name}
+      tmp.each_with_index do |song, index|
+        puts ((index + 1).to_s + song.name)
     else
       nil
     end
