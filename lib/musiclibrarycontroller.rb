d6b9dd8d1d36257878ff777ecc1703cbe84d9421
Plimon Howard <plimon.howard@gmail.com>
2020-10-22 19:45:44 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -62,7 +62,7 @@ class MusicLibraryController
     if (artist != nil)
       tmp = artist.songs.sort_by { |songs| songs.name}
       tmp.each_with_index do |song, index|
-        puts ((index + 1).to_s +". " + song.name)
+        puts ((index + 1).to_s + ". " + song.name)
       end
     else
       nil
